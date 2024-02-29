9b58a4c67e1ee1776dfb5c84a2cd4fbb67cba18e
Jaff_FR <161663111+JafFR13@users.noreply.github.com>
2024-02-29 09:04:28 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1709166794,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/backgroundday-sheet0.png",
+		"images/yellowbirdmidflap-sheet0.png",
+		"images/yellowbirdmidflap-sheet1.png",
+		"images/pipegreen-sheet0.png",
+		"images/pipegreendown-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
